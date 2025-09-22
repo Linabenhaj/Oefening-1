682c2380b2977614e4c80c577c960e4efb826d58
Linabenhaj <Benhaj.L@outlook.com>
2025-09-23 00:31:12 +0200

    Projectbestanden toegevoegd, juiste mapstructuur

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Oefening_1
+{
+    internal class Program
+    {
+        // Hoofdmethode
+        static void Main(string[] args)
+        {
+            // Tekst van gebruiker vragen
+            Console.Write("Geef de tekst die je wil omkeren: ");
+            string tekst = Console.ReadLine();
+
+            // Menu tonen en keuze van gebruiker
+            Console.WriteLine("\nKies een methode:");
+            Console.WriteLine("1 = For-loop");
+            Console.WriteLine("2 = While-loop");
+            Console.WriteLine("3 = Do-while-loop");
+            Console.WriteLine("4 = Recursie");
+            Console.Write("\nUw keuze: ");
+            string keuze = Console.ReadLine();
+
+            string resultaat = "";
+    }
+    }
